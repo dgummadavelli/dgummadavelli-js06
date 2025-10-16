4bc540539316a68b6e2a5cd4040fdda6e31f3ab4
Dinesh Kumar Gummdavelli <dgummadavelli@uco.edu>
2025-10-15 20:34:54 -0500

    Assignment 6 changes & updates

---
+++ project02/project06-02.js
@@ -0,0 +1,23 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-02
+
+      Project to turn a selection list into a selection of hypertext links
+      Author: Dinesh Kumar Gummadavelli
+      Date:   10/15/2025
+
+      Filename: project06-02.js
+*/
+
+window.addEventListener("load", function() {
+   let allSelect = document.querySelectorAll("form#govLinks select");
+   for (let i = 0; i < allSelect.length; i++) {
+      allSelect[i].onchange = function(evt) {
+         let linkURL = evt.target.value;
+         let newWin = window.open(linkURL);
+      };
+   }
+});
+
+
